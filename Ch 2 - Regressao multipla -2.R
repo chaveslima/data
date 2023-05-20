08a945436c43466da333a23a395b6e2accb886e4
chaveslima <49008746+chaveslima@users.noreply.github.com>
2023-05-20 20:45:51 -0300

    Create Ch 2 - Regressao multipla -2.R

---
+++ Ch 2 - Regressao multipla -2.R
@@ -0,0 +1,15 @@
+library(WDI)
+### GDP  (constant 2015 US$) - trilhoes
+Y=WDI(country="KOR", indicator = "NY.GDP.MKTP.KD");Y
+y=Y[2:33,5]/(10**12); y; plot.ts(rev(y))
+#
+### Labor people 15 ou mais - milhoes 
+L=WDI(country="KOR", indicator = "SL.TLF.TOTL.IN"); L
+l=L[2:33,5]/(10**6); l; plot.ts(rev(l))
+#
+### Gross capital formation (Constante 2015 US$) - bilhoes
+K=WDI(country="KOR", indicator = "NE.GDI.TOTL.KD");K
+k=K[2:33,5]/(10**12); plot.ts(rev(k)); k
+#
+reg=lm(y~l+k)
+summary(reg)
